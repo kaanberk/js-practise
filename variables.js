e16832f3673d289be6544bf99e7327d7784cd8dd
Kaan Berk Kocak <kaankocak@Kaans-MacBook-Pro.local>
2021-06-28 14:46:29 +0100

    First Commit

---
+++ variables.js
@@ -0,0 +1,42 @@
+
+var name;
+name = " Kaan Berk Kocak";
+
+var address = "26 London Road, Oadby, Leicester";
+
+var age = 123;
+
+console.log(name)
+console.log(address)
+console.log(age)
+
+var firstname = "Jack",
+    lastname = "Holmes";
+
+console.log(firstname)
+console.log(lastname)
+
+/*
+
+    Notes : 
+
+- Variable names can begin with letter, or the symbols $ or _. They can only contain letters, numbers, $ and _. 
+  They cannot begin with a number. "myVarable", "leaf_1", and "$money3" are all examples of valud variable names.
+
+  var $_na8me = "Adam";
+
+- Variable names are case sensitive, which means that"xPos" is different from "xpos", so make sure you are consistent.
+
+  var firstname = "Adam";
+  console.log(firstNAME); -> error
+
+- Variable names should use camel case for multiple words, like "toothSize" instead of "tootsize" or "tooth_size".
+
+  var ninumberofemployee = 1234451
+  var NInumberOfEmployee = 1234451
+
+- variable cannot be any of Javascripst's reserved words or keywords. 
+
+  var function = "something"  --> error because it is a reseved word 
+  
+*/
